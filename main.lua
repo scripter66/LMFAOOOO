9fdae949d5932b4b85a5dd7de5fc706d07f05490
sc.ripter <115365072+scripter66@users.noreply.github.com>
2024-08-22 16:13:53 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,15 +6,4 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Anonyme", -- Required
 	Text = "Welcome,"..game.Players.LocalPlayer.Name, -- Required
 })
-getgenv().queue_on_teleport = function(scripttoexec) -- WARNING: MUST HAVE MOREUNC IN AUTO EXECUTE FOR THIS TO WORK.
- local newTPService = {
-  __index = function(self, key)
-   if key == 'Teleport' then
-    return function(gameId, player, teleportData, loadScreen)
-      teleportData = {teleportData, MOREUNCSCRIPTQUEUE=scripttoexec}
-      return oldGame:GetService("TeleportService"):Teleport(gameId, player, teleportData, loadScreen)
-    end
-   end
-  end
- 
 
