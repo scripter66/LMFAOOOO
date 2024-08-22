fb7b2a2e9e620043c1906bc9136b1cf0762bf486
sc.ripter <115365072+scripter66@users.noreply.github.com>
2024-08-22 09:11:12 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,33 @@
-local v0=tonumber;local v1=string.byte;local v2=string.char;local v3=string.sub;local v4=string.gsub;local v5=string.rep;local v6=table.concat;local v7=table.insert;local v8=math.ldexp;local v9=getfenv or function() return _ENV;end ;local v10=setmetatable;local v11=pcall;local v12=select;local v13=unpack or table.unpack ;local v14=tonumber;local function v15(v16,v17,...) local v18=1;local v19;v16=v4(v3(v16,5),"..",function(v30) if (v1(v30,2)==79) then local v86=0;while true do if (v86==0) then v19=v0(v3(v30,1,1));return "";end end else local v87=v2(v0(v30,16));if v19 then local v111=v5(v87,v19);v19=nil;return v111;else return v87;end end end);local function v20(v31,v32,v33) if v33 then local v88=(0 + 0) -0 ;local v89;while true do if (v88==0) then v89=(v31/(2^(v32-(2 -1))))%(2^(((v33-(1 -0)) -(v32-(2 -1))) + (620 -(555 + 64)))) ;return v89-(v89%(932 -((1221 -364) + 74))) ;end end else local v90=0;local v91;while true do if (v90==((2205 -(1523 + 114)) -(367 + 201))) then v91=((1994 -(68 + 997)) -(214 + 713))^(v32-(1 + (1270 -(226 + 1044)))) ;return (((v31%(v91 + v91))>=v91) and (1 + 0)) or (877 -(282 + 595)) ;end end end end local function v21() local v34=v1(v16,v18,v18);v18=v18 + 1 ;return v34;end local function v22() local v35,v36=v1(v16,v18,v18 + (119 -(32 + 85)) );v18=v18 + (8 -6) ;return (v36 * (251 + 5)) + v35 ;end local function v23() local v37=0 + 0 ;local v38;local v39;local v40;local v41;while true do if ((958 -(892 + 65))==v37) then return (v41 * 16777216) + (v40 * (48056 + 17480)) + (v39 * (610 -(952 -598))) + v38 ;end if (v37==0) then v38,v39,v40,v41=v1(v16,v18,v18 + (5 -2) );v18=v18 + (15 -11) ;v37=1 -0 ;end end end local function v24() local v42=v23();local v43=v23();local v44=443 -(416 + 26) ;local v45=(v20(v43,1 -0 ,9 + 11 ) * ((2 + 0)^(56 -24))) + v42 ;local v46=v20(v43,1018 -(915 + (967 -(261 + 624))) ,(819 -358) -(44 + 386) );local v47=((v20(v43,90 -58 )==(1 + 0)) and  -(1 -0)) or (1188 -(1069 + 118)) ;if (v46==((2218 -(1020 + 60)) -(116 + 1022))) then if (v45==(0 -0)) then return v47 * (0 + 0) ;else v46=1 -0 ;v44=(1423 -(630 + 793)) + 0 ;end elseif (v46==(7269 -5222)) then return ((v45==((0 -0) -0)) and (v47 * (
+print("Anonyme @ V2")
+print("Welcome,"..game.Players.LocalPlayer.Name)
+print("Owner @ sc.ripter | Flamby @ Second Owner @ heclome | Nova")
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Anonyme", -- Required
+	Text = "Welcome,"..game.Players.LocalPlayer.Name, -- Required
+})
+getgenv().queue_on_teleport = function(scripttoexec) -- WARNING: MUST HAVE MOREUNC IN AUTO EXECUTE FOR THIS TO WORK.
+ local newTPService = {
+  __index = function(self, key)
+   if key == 'Teleport' then
+    return function(gameId, player, teleportData, loadScreen)
+      teleportData = {teleportData, MOREUNCSCRIPTQUEUE=scripttoexec}
+      return oldGame:GetService("TeleportService"):Teleport(gameId, player, teleportData, loadScreen)
+    end
+   end
+  end
+ }
+ local gameMeta = {
+  __index = function(self, key)
+    if key == 'GetService' then
+     return function(name)
+      if name == 'TeleportService' then return newTPService end
+     end
+    elseif key == 'TeleportService' then return newTPService end
+    return game[key]
+  end,
+  __metatable = 'The metatable is protected'
+ }
+ getgenv().game = setmetatable({}, gameMeta)
+end
+getgenv().queueonteleport = queue_on_teleport
