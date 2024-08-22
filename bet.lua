bc9f537fa99fbd5f1919b90afdb44c35049d11be
sc.ripter <115365072+scripter66@users.noreply.github.com>
2024-08-22 12:47:29 +0200

    Create bet.lua

---
+++ bet.lua
@@ -0,0 +1,29 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "GoatSploit", -- Required
+	Text = "Welcome,"..game.Players.LocalPlayer.Name, -- Required
+})
+getgenv().queue_on_teleport = function(scripttoexec) -- WARNING: MUST HAVE MOREUNC IN AUTO EXECUTE FOR THIS TO WORK.
+ local newTPService = {
+  __index = function(self, key)
+   if key == 'Teleport' then
+    return function(gameId, player, teleportData, loadScreen)
+      teleportData = {teleportData, MOREUNCSCRIPTQUEUE=scripttoexec}
+      return oldGame:GetService("TeleportService"):Teleport(gameId, player, teleportData, loadScreen)
+    end
+   end
+  end
+ }
+ local gameMeta = {
+  __index = function(self, key)
+    if key == 'GetService' then
+     return function(name)
+      if name == 'TeleportService' then return newTPService end
+     end
+    elseif key == 'TeleportService' then return newTPService end
+    return game[key]
+  end,
+  __metatable = 'The metatable is protected'
+ }
+ getgenv().game = setmetatable({}, gameMeta)
+end
+getgenv().queueonteleport = queue_on_teleport
